c6f76c7ce65cdc1d0088bb9fd7ec84a4fa4134e5
Masayoshi Wada <developer@andantesoftware.com>
2014-05-17 01:06:56 +0900

    update v1.3

--- chikubeam.rb
+++ chikubeam.rb
@@ -2,13 +2,14 @@ require "formula"
 
 class Chikubeam < Formula
   homepage "https://github.com/masawada/chikubeam"
-  url "https://github.com/masawada/chikubeam/archive/v1.2.tar.gz"
-  sha256 "606d00bc4736ef3fe10fdaa554985a08fd5642279a96f30ab2727b2cc7a771c1"
+  url "https://github.com/masawada/chikubeam/archive/v1.3.tar.gz"
+  sha256 "a53bfbbaa69163c3bac8010df4e1a37dd36afbedeee91d584755081385e0066f"
   head "https://github.com/masawada/chikubeam.git"
-  version "1.2"
+  version "1.3"
 
   def install
     system "make"
     bin.install "chikubeam"
+    man1.install "chikubeam.1"
   end
 end
